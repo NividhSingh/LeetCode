fd9879698bb3a319899ca6dbec9cbbf6954072f2
Nividh S <67477539+NividhSingh@users.noreply.github.com>
2025-06-20 21:56:15 -0700

    Create 1672.cpp

---
+++ 1672/1672.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth = -1;
+        for (int i = 0; i < accounts.size(); i++) {
+            int wealth = 0;
+            for (int j = 0; j < accounts[0].size(); j++) {
+                wealth += accounts[i][j];
+            }
+            maxWealth = max(maxWealth, wealth);
+        }
+        return maxWealth;
+    }
+};
