ada6742a4e0510d5db111054adf434d9ad72359d
Nividh S <67477539+NividhSingh@users.noreply.github.com>
2025-06-20 21:47:48 -0700

    Create 1470.cpp

---
+++ 1470/1470.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(nums.size());
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+        }
+        for (int i = n; i < 2 * n; i++) {
+            ans[2 * (i - n) + 1] = nums[i];
+        }
+        return ans;
+    }
+};
