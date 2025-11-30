d0373e3a8b596354dbf5630267a3649b40a3e437
Nividh S <67477539+NividhSingh@users.noreply.github.com>
2025-11-29 23:12:08 -0500

    Create 3760.cpp

---
+++ 3760/3760.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        vector<int> count(26);
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+        }
+        int m = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                m++;
+            }
+        }
+        return m;
+    }
+};
