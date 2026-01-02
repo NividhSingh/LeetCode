61f2beb958c5eff535d8170363188e9c8cce7287
Nividh S <67477539+NividhSingh@users.noreply.github.com>
2026-01-01 19:50:30 -0800

    Create 961.cpp

---
+++ 961/961.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) == s.end()) {
+                s.insert(nums[i]);
+            }
+            else {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
